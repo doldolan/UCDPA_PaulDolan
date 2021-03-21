9a9f11cbbad0570ec2e890d85fc84dcc0b202da0
Paul Dolan <doldolan@gmail.com>
2021-03-21 17:46:36 +0000

    Project Start

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-print("Dolan")
+import matplotlib as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
-print("Galway")
+Customers = pd.read_csv(r"C:\Users\Paul Dolan\Downloads\UCD Project\CUSTOMER.csv")
+Orders_Fact = pd.read_csv(r"C:\Users\Paul Dolan\Downloads\UCD Project\ORDER_FACT.csv")
+Product_Dim = pd.read_csv(r"C:\Users\Paul Dolan\Downloads\UCD Project\PRODUCT_DIM.csv")
+
+# Checking to see if there is any blank values in any of the Customers columns
+Customers.info()
+Customers.dropna(axis=1, inplace=True)
+
+# Counting missing values in each column in Orders_Fact table
+print(Orders_Fact.isna().sum())
+Orders_Fact['Discount'] = Orders_Fact['Discount'].fillna(0)
+print(Orders_Fact.isna().sum())
